ea2531504044f26fef4aef6bd59d90da08da6406
Ashok Kumar Verma <ashokv1337@gmail.com>
2022-01-13 10:02:46 +0530

    modified
    updated

--- lib/home.dart
+++ lib/home.dart
@@ -2,85 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:otp_auth_flutter/otpscreen.dart';
 
-class homepage extends StatefulWidget {
-  const homepage({Key? key}) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
-  _homepageState createState() => _homepageState();
+  _HomepageState createState() => _HomepageState();
 }
 
-class _homepageState extends State<homepage> {
+class _HomepageState extends State<Homepage> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   TextEditingController phonecontroller = TextEditingController();
   TextEditingController otpcontroller = TextEditingController();
   String verificationID = "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.pink[600], borderRadius: BorderRadius.circular(54)),
-        height: 500,
-        width: 500,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 200,
-            ),
-            Text(
-              "Hello! Champion",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40)),
-                child: TextField(
-                  controller: phonecontroller,
-                  decoration: InputDecoration(
-                    prefix: Text("+91",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.black,
-                        )),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 5),
-                      borderRadius: BorderRadius.all(Radius.circular(40)),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.green[900],
+                  borderRadius: BorderRadius.circular(54)),
+              height: 500,
+              width: 500,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 200,
+                  ),
+                  Text(
+                    "Hello! Welcome",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(40)),
+                      child: TextField(
+                        controller: phonecontroller,
+                        decoration: InputDecoration(
+                          prefix: Text("+91",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.black,
+                              )),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: getOtp,
+                      child: Text("Login"),
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: Colors.yellow[700],
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 155, vertical: 15),
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                          ))),
+                  SizedBox(height: 15),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forgot password ?",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ))
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: getOtp,
-                child: Text("Login"),
-                style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    primary: Colors.yellow[700],
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 155, vertical: 15),
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                    ))),
-            SizedBox(height: 15),
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Forgot password ?",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
-                ))
-          ],
+          ),
         ),
       ),
     );
--- lib/otpscreen.dart
+++ lib/otpscreen.dart
@@ -22,80 +22,87 @@ class _OtpScreenState extends State<OtpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.pink[600], borderRadius: BorderRadius.circular(54)),
-        height: 500,
-        width: 500,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 200,
-            ),
-            Text(
-              "Enter OTP",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40)),
-                child: TextField(
-                  controller: otpcontroller,
-                  decoration: InputDecoration(
-                    labelText: "Enter OTP",
-                    labelStyle: TextStyle(fontSize: 19),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 5),
-                      borderRadius: BorderRadius.all(Radius.circular(40)),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.pink[600],
+                  borderRadius: BorderRadius.circular(54)),
+              height: 500,
+              width: 500,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 200,
+                  ),
+                  Text(
+                    "Enter OTP",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(40)),
+                      child: TextField(
+                        controller: otpcontroller,
+                        decoration: InputDecoration(
+                          labelText: "Enter OTP",
+                          labelStyle: TextStyle(fontSize: 19),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 5),
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {
+                        AuthCredential phoneAuthCredential =
+                            PhoneAuthProvider.credential(
+                                verificationId: verificationID,
+                                smsCode: otpcontroller.text);
+                        signin(phoneAuthCredential);
+                      },
+                      child: Text("Submit"),
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: Colors.yellow[700],
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 152, vertical: 15),
+                          textStyle: TextStyle(
+                            fontSize: 20,
+                          ))),
+                  SizedBox(height: 19),
+                  Text(
+                    "Haven't recieved OTP?",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Resend",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ))
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  AuthCredential phoneAuthCredential =
-                      PhoneAuthProvider.credential(
-                          verificationId: verificationID,
-                          smsCode: otpcontroller.text);
-                  signin(phoneAuthCredential);
-                },
-                child: Text("Submit"),
-                style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    primary: Colors.yellow[700],
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 152, vertical: 15),
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                    ))),
-            SizedBox(height: 19),
-            Text(
-              "Haven't recieved OTP?",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold),
-            ),
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Resend",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
-                ))
-          ],
+          ),
         ),
       ),
     );
